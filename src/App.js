5fad07ddec982458a65880b45fe57db957462ed8
Dima <leedmitry8091@gmail.com>
2024-03-14 23:31:03 +1100

    Lesson 11: add first header, nav bar and main

--- src/App.js
+++ src/App.js
@@ -1,13 +1,47 @@
 import React from "react";
 import './App.css';
-import Header from './Header'
-import Techno from "./Techno";
+import logo from "./logo.png"
 
 const App = () => {
     return (
-        <div>
-            <Header/>
-            <Techno/>
+        <div className='app-wrapper'>
+            <header className='header'>
+                <img src={logo} alt="logo"/>
+            </header>
+            <nav className='nav'>
+                <div>
+                    <a href="#">Profile</a>
+                </div>
+                <div>
+                    <a href="#">News</a>
+                </div>
+                <div>
+                    <a href="#">Massages</a>
+                </div>
+                <div>
+                    <a href="#">Music</a>
+                </div>
+                <div>
+                    <a href="#">Settings</a>
+                </div>
+            </nav>
+            <main className='main'>
+                <div>
+                    banner
+                </div>
+                <div>
+                    ava + description
+                </div>
+                <div>
+                    my posts
+                    <div>
+                        new posts
+                    </div>
+                    <div>
+                        post 1
+                    </div>
+                </div>
+            </main>
         </div>
     );
 }
