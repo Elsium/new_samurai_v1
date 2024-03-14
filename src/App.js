060aeb11b78895bd8e1d254e34c1ea4360427a24
Dima <leedmitry8091@gmail.com>
2024-03-14 20:12:00 +1100

    Lesson 7: create components

--- src/App.js
+++ src/App.js
@@ -4,9 +4,30 @@ import './App.css';
 const App = () => {
   return (
       <div>
-
+          <Header />
+          <Techno />
       </div>
-  )
+  );
 }
-
+const Techno = () => {
+    return (
+        <div>
+            <ul>
+                <li>css</li>
+                <li>html</li>
+                <li>js</li>
+                <li>react</li>
+            </ul>
+        </div>
+    )
+}
+const Header = () => {
+    return (
+        <div>
+            <a href="http://google.com">Home </a>
+            <a href="http://google.com">News </a>
+            <a href="http://google.com">Messages</a>
+        </div>
+    );
+}
 export default App;
