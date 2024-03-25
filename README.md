38dffc5b740940aef4ce96b96514cb07b07bd3b7
Dima <leedmitry8091@gmail.com>
2024-03-25 22:02:48 +1100

    Lesson 21: add dialogs page

--- README.md
+++ README.md
@@ -7,3 +7,4 @@
 5. **21.03.2024** - reached the 15th lesson
 6. **22.03.2024** - reached the 17.xth lesson
 7. **23.03.2024** - reached the 20th lesson
+8. **25.03.2024** - reached the 21th lesson
--- src/components/Dialogs/Dialogs.jsx
+++ src/components/Dialogs/Dialogs.jsx
@@ -3,8 +3,193 @@ import style from "./Dialogs.module.scss"
 
 const Dialogs = () => {
     return (
-        <section>
-            dialogs
+        <section className={style.container}>
+            <div>
+                <div className={style.find}>
+                    find dialog
+                </div>
+                <div className={style.dialogsItems}>
+                    <div className={style.dialog}>
+                        Ivan Dog
+                    </div>
+                    <div className={style.dialog}>
+                        Lisa Cat
+                    </div>
+                    <div className={style.dialog}>
+                        Marina El
+                    </div>
+                    <div className={style.dialog}>
+                        Ivan Dog
+                    </div>
+                    <div className={style.dialog}>
+                        Lisa Cat
+                    </div>
+                    <div className={style.dialog}>
+                        Marina El
+                    </div>
+                    <div className={style.dialog}>
+                        Ivan Dog
+                    </div>
+                    <div className={style.dialog}>
+                        Lisa Cat
+                    </div>
+                    <div className={style.dialog}>
+                        Marina El
+                    </div>
+                    <div className={style.dialog}>
+                        Ivan Dog
+                    </div>
+                    <div className={style.dialog}>
+                        Lisa Cat
+                    </div>
+                    <div className={style.dialog}>
+                        Marina El
+                    </div>
+                    <div className={style.dialog}>
+                        Ivan Dog
+                    </div>
+                    <div className={style.dialog}>
+                        Lisa Cat
+                    </div>
+                    <div className={style.dialog}>
+                        Marina El
+                    </div>
+                    <div className={style.dialog}>
+                        Ivan Dog
+                    </div>
+                    <div className={style.dialog}>
+                        Lisa Cat
+                    </div>
+                    <div className={style.dialog}>
+                        Marina El
+                    </div>
+                    <div className={style.dialog}>
+                        Ivan Dog
+                    </div>
+                    <div className={style.dialog}>
+                        Lisa Cat
+                    </div>
+                    <div className={style.dialog}>
+                        Marina El
+                    </div>
+                </div>
+            </div>
+            <div>
+                <div className={style.dialogUser}>
+                    <p>ava + name</p>
+                    <p>additional</p>
+                </div>
+                <div className={style.messagesItems}>
+                    <div className={style.message}>
+                        Hi
+                    </div>
+                    <div className={style.message}>
+                        Hi
+                    </div>
+                    <div className={style.message}>
+                        Ok
+                    </div>
+                    <div className={style.message}>
+                        Bye
+                    </div>
+                    <div className={style.message}>
+                        Hi
+                    </div>
+                    <div className={style.message}>
+                        Hi
+                    </div>
+                    <div className={style.message}>
+                        Ok
+                    </div>
+                    <div className={style.message}>
+                        Bye
+                    </div>
+                    <div className={style.message}>
+                        Hi
+                    </div>
+                    <div className={style.message}>
+                        Hi
+                    </div>
+                    <div className={style.message}>
+                        Ok
+                    </div>
+                    <div className={style.message}>
+                        Bye
+                    </div>
+                    <div className={style.message}>
+                        Hi
+                    </div>
+                    <div className={style.message}>
+                        Hi
+                    </div>
+                    <div className={style.message}>
+                        Ok
+                    </div>
+                    <div className={style.message}>
+                        Bye
+                    </div>
+                    <div className={style.message}>
+                        Hi
+                    </div>
+                    <div className={style.message}>
+                        Hi
+                    </div>
+                    <div className={style.message}>
+                        Ok
+                    </div>
+                    <div className={style.message}>
+                        Bye
+                    </div>
+                    <div className={style.message}>
+                        Hi
+                    </div>
+                    <div className={style.message}>
+                        Hi
+                    </div>
+                    <div className={style.message}>
+                        Ok
+                    </div>
+                    <div className={style.message}>
+                        Bye
+                    </div>
+                    <div className={style.message}>
+                        Hi
+                    </div>
+                    <div className={style.message}>
+                        Hi
+                    </div>
+                    <div className={style.message}>
+                        Ok
+                    </div>
+                    <div className={style.message}>
+                        Bye
+                    </div>
+                    <div className={style.message}>
+                        Hi
+                    </div>
+                    <div className={style.message}>
+                        Hi
+                    </div>
+                    <div className={style.message}>
+                        Ok
+                    </div>
+                    <div className={style.message}>
+                        Bye
+                    </div>
+                    <div className={style.message}>
+                        Hi
+                    </div>
+                    <div className={style.message}>
+                        Hi
+                    </div>
+                    <div className={style.message}>
+                        Ok
+                    </div>
+                    <div className={style.message}>
+                        Bye
+                    </div>
+                </div>
+            </div>
         </section>
     );
 }
