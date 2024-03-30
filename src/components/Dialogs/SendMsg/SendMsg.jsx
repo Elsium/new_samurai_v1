b063f2ffe69b88966ed53c122b1ffd0bc91bf10f
Dima <leedmitry8091@gmail.com>
2024-03-30 20:37:48 +1100

    Lesson 31: add onClick event to msg

--- src/components/Dialogs/SendMsg/SendMsg.jsx
+++ src/components/Dialogs/SendMsg/SendMsg.jsx
@@ -3,11 +3,19 @@ import style from "./SendMsg.module.scss"
 import send from "./img/send.png"
 
 const SendMsg = (props) => {
+
+    let msgElement = React.createRef();
+
+    let sendMsg = () => {
+        let text = msgElement.current.value;
+        alert(text)
+    }
+
     return (
         <div className={style.container}>
             <div>
-                <textarea placeholder="Type your message..."></textarea>
-                <div>
+                <textarea ref={msgElement} placeholder="Type your message..."></textarea>
+                <div onClick={sendMsg}>
                     <img src={send} alt=""/>
                 </div>
             </div>
