ee65378b8b5438fbbb1e07b521049ef575be6d6d
Dima <leedmitry8091@gmail.com>
2024-03-31 18:03:20 +1100

    add styles for tab switch

--- src/components/Dialogs/Dialog/Dialog.jsx
+++ src/components/Dialogs/Dialog/Dialog.jsx
@@ -4,8 +4,8 @@ import {NavLink} from "react-router-dom";
 
 const Dialog = (props) => {
     return (
-        <NavLink to={'/dialog/' + props.userID}>
-            <div className={style.dialog}>{props.name}</div>
+        <NavLink to={'/dialog/' + props.userID} className={style.dialog}>
+            <div>{props.name}</div>
         </NavLink>
     );
 }
