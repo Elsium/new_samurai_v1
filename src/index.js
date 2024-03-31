1a6e71770a0b0c465ac76c56de32de157d9abd4a
Dima <leedmitry8091@gmail.com>
2024-03-31 17:06:45 +1100

    Lesson 36: rename state.js to store.js and refactor: create object of store

--- src/index.js
+++ src/index.js
@@ -2,7 +2,7 @@ import React from 'react';
 import ReactDOM from 'react-dom/client';
 import {BrowserRouter} from 'react-router-dom'
 import './index.scss';
-import state, {subscribe, addPost, updatePost, sendMsg, updateMsg} from './redux/state'
+import store from './redux/store'
 import App from './App';
 
 const root = ReactDOM.createRoot(document.getElementById('root'));
@@ -11,11 +11,15 @@ export let renderEntireTree = (state) => {
     root.render(
         <React.StrictMode>
             <BrowserRouter>
-                <App state={state} addPost={addPost} updatePost={updatePost} sendMsg={sendMsg} updateMsg={updateMsg}/>
+                <App state={state}
+                     addPost={store.addPost.bind(store)}
+                     updatePost={store.updatePost.bind(store)}
+                     sendMsg={store.sendMsg.bind(store)}
+                     updateMsg={store.updateMsg.bind(store)}/>
             </BrowserRouter>
         </React.StrictMode>
     );
 }
-renderEntireTree(state);
+renderEntireTree(store.getState());
 
-subscribe(renderEntireTree);
+store.subscribe(renderEntireTree);
---
+++ src/redux/store.js
@@ -0,0 +1,102 @@
+let store = {
+    _state: {
+        profileData: {
+            posts: [
+                {
+                    postID: 2,
+                    name: 'Diana Fox',
+                    text: 'Hello',
+                    date: '23.03.2024',
+                    time: '15:32',
+                    likesCount: 96,
+                    commentsCount: 10
+                },
+                {
+                    postID: 1,
+                    name: 'Diana Fox',
+                    text: 'It is my first post here',
+                    date: '20.03.2024',
+                    time: '19:23',
+                    likesCount: 115,
+                    commentsCount: 15
+                }
+            ],
+            currentPostText: "",
+        },
+        dialogsData: {
+            msg: [
+                {userID: 1, msgID: 8, text: 'ok', time: '12:34', from: 1},
+                {userID: 4, msgID: 7, text: 'i`m so sorry (((', time: '12:33', from: 0},
+                {userID: 1, msgID: 6, text: 'because you were ignore me...', time: '12:33', from: 1},
+                {userID: 4, msgID: 5, text: 'Why?', time: '12:32', from: 0},
+                {userID: 1, msgID: 4, text: 'Bye', time: '12:14', from: 1},
+                {userID: 1, msgID: 3, text: 'Ok', time: '12:14', from: 1},
+                {userID: 4, msgID: 2, text: 'Hi', time: '12:14', from: 0},
+                {userID: 1, msgID: 1, text: 'Hi', time: '12:12', from: 1},
+            ],
+            dialogs: [
+                {userID: 1, name: 'Ivan Dog'},
+                {userID: 2, name: 'Lisa Cat'},
+                {userID: 3, name: 'Marina El'}
+            ],
+            currentMsgText: "",
+        }
+    },
+    getState() {
+        return this._state;
+    },
+    _callSubscriber() {
+        console.log('not connected');
+    },
+    addPost() {
+        if (this._state.profileData.currentPostText === "") return
+
+        const time = new Date();
+        let tempDate = ("0" + time.getDate()).slice(-2) + "." + ("0" + (time.getMonth() + 1)).slice(-2) + "." + time.getFullYear();
+        let tempTime = ("0" + time.getHours()).slice(-2) + ":" + ("0" + time.getMinutes()).slice(-2);
+
+        let newPost = {
+            postID: this._state.profileData.posts.length + 1,
+            name: 'Diana Fox',
+            text: this._state.profileData.currentPostText,
+            date: tempDate,
+            time: tempTime,
+            likesCount: 0,
+            commentsCount: 0
+        }
+        this._state.profileData.posts = [newPost, ...this._state.profileData.posts];
+        this._state.profileData.currentPostText = "";
+        this._callSubscriber(this._state);
+    },
+    updatePost(newText) {
+        this._state.profileData.currentPostText = newText;
+        this._callSubscriber(this._state);
+    },
+    sendMsg() {
+        if (this._state.dialogsData.currentMsgText === "") return
+
+        const time = new Date();
+        let tempTime = ("0" + time.getHours()).slice(-2) + ":" + ("0" + time.getMinutes()).slice(-2);
+
+        let newMsg = {
+            userID: 4,
+            msgID: this._state.dialogsData.msg.length + 1,
+            text: this._state.dialogsData.currentMsgText,
+            time: tempTime,
+            from: 0
+        };
+
+        this._state.dialogsData.msg = [newMsg, ...this._state.dialogsData.msg];
+        this._state.dialogsData.currentMsgText = "";
+        this._callSubscriber(this._state);
+    },
+    updateMsg(newText) {
+        this._state.dialogsData.currentMsgText = newText;
+        this._callSubscriber(this._state);
+    },
+    subscribe(observer) {
+        this._callSubscriber = observer;
+    },
+}
+
+export default store;
