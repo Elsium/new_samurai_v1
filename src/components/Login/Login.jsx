b384f402774b025a1381b2328cad321591d4ae79
Dima <leedmitry8091@gmail.com>
2024-04-16 00:27:34 +1100

    Lesson 75: add redux-form and login form (need to change redux-form to something new..)

--- src/components/Login/Login.jsx
+++ src/components/Login/Login.jsx
@@ -1,9 +1,43 @@
 import React from 'react';
+import {Field, reduxForm} from 'redux-form';
+import style from './Login.module.scss';
+
+const LoginForm = (props) => {
+    return (
+        <form onSubmit={props.handleSubmit} className={style.form}>
+            <div className={style.input}>
+                <Field component={'input'} placeholder={'Login'} name={'login'}/>
+            </div>
+            <div className={style.input}>
+                <Field component={'input'} placeholder={'Password'} name={'password'}/>
+            </div>
+            <div className={style.checkbox}>
+                <label>
+                    <Field component={'input'} type={'checkbox'} name={'rememberMe'} id={'rememberMe'}/> <p style={{marginLeft: '10px', userSelect: 'none'}}>RememberMe</p>
+                </label>
+            </div>
+            <div className={style.btn}>
+                <button>Login</button>
+            </div>
+        </form>
+    )
+}
+
+const LoginReduxForm = reduxForm({form: 'login'})(LoginForm)
 
 const Login = () => {
+    const onSubmit = (formData) => {
+        console.log(formData)
+    }
+
     return (
-        <div>
-            LOGIN
+        <div className={style.wrapper}>
+            <h1 className={style.title}>Login</h1>
+            <LoginReduxForm onSubmit={onSubmit}/>
+            <div className={style.info}>
+                <h2>Info</h2>
+                <p>Lorem ipsum dolor sit amet, consectetur adipisicing elit. Asperiores assumenda consectetur facilis laboriosam modi vel velit voluptates voluptatum. At dicta excepturi inventore reiciendis sint. Consequuntur delectus esse iste itaque sequi?</p>
+            </div>
         </div>
     );
 };
--- src/redux/reduxStore.js
+++ src/redux/reduxStore.js
@@ -4,12 +4,14 @@ import dialogsReducer from './dialogsReducer';
 import profileReducer from './profileReducer';
 import usersReducer from './usersReducer';
 import authReducer from './authReducer';
+import { reducer as formReducer } from 'redux-form'
 
 let reducers = combineReducers({
     profileData: profileReducer,
     dialogsData: dialogsReducer,
     usersPage: usersReducer,
     auth: authReducer,
+    form: formReducer,
 });
 
 let store = createStore(reducers, applyMiddleware(thunk));
