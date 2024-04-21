f1164d60f8f272a0ed1b898b9e0378d3ac77d026
Dima <leedmitry8091@gmail.com>
2024-04-21 17:53:47 +1100

    replace img to assets

--- src/components/Header/Header.jsx
+++ src/components/Header/Header.jsx
@@ -1,5 +1,5 @@
 import React from 'react';
-import logo from '../../img/logo.png';
+import logo from '../../assets/img/logo.png';
 import style from './Header.module.scss';
 import {NavLink} from 'react-router-dom';
 import LoginButton from '../UI/LoginBtn/LoginBtn';
